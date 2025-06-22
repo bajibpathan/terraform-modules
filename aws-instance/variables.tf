78223a2f7959f26a6f47ad155b8fddbcba0fd160
Baji Pathan <bajibpathan@gmail.com>
2025-06-22 07:54:40 -0400

    aws-instance module update

---
+++ aws-instance/variables.tf
@@ -0,0 +1,26 @@
+variable "ami_id" {
+  type  = string
+  description = "AMI ID of the EC2 instance"
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+    description = "EC2 Instance size"
+
+    validation {
+      condition = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
+      error_message = "Valid values for instance_type are: t3.micro, t3.small, t3.medium"
+    }
+}
+
+# mandatory value
+variable "sg_ids" {
+    type = list
+}
+
+variable "tags" {
+    type = map
+  
+}
